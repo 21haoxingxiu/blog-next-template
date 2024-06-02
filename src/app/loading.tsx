c93bd29214b3dd7e6caced8a56e7a869617529b2
21haoxingxiu <1047918517@qq.com>
2024-06-02 15:34:58 +0800

    fix: loading优化

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,9 @@
 import Loading from '~/components/common/loading'
 
-export default Loading
+export default function RooLoading() {
+  return <div className=' w-full h-[80vh] center'>
+    <div className=' w-[300px] h-[300px]'>
+      <Loading />
+    </div>
+  </div>
+}
